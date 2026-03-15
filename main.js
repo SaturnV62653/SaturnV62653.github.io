c18d572525ea5eb787034e0d4bbcfc4309d2a4d9
SaturnV62653 <galenstasonis@icloud.com>
2026-03-15 15:48:27 -0400

    dark mode fix maybe???

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function switchTheme() {
-    alert('test');
+    const body = document.body;
+
+    if (body.classList.contains('dark')) {
+        body.classList.remove('dark');
+        body.classList.add('dark');
+    }
 }
